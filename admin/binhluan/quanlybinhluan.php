7075b76ebc4278ff6f595d123c943a2091f495c3
MaiLe <lephuongmaie@gmail.com>
2023-05-21 11:33:14 +0700

    add project

---
+++ admin/binhluan/quanlybinhluan.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_SESSION['user'])){
+    extract($_SESSION['user']);
+}
+?>
+<div class="main-content">
+    <div class="row">
+        <div class="col-md-6 col-sm-8 clearfix">
+            <ul class="user-info pull-left pull-none-xsm">
+                <li class="profile-info dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> <img
+                                src="https://demo.neontheme.com/assets/images/thumb-1@2x.png" alt="" class="img-circle"
+                                width="44"/>
+                        <?=$hoten?>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-6 col-sm-4 clearfix hidden-xs">
+            <ul class="list-inline links-list pull-right">
+                <li class="sep"></li>
+                <li><a href="#">
+                        Đăng xuất <i class="entypo-logout right"></i> </a></li>
+            </ul>
+        </div>
+    </div>
+    <hr/>
+    <ol class="breadcrumb bc-3">
+        <li><a href="£"><i class="fa fa-home"></i>Trang Admin</a></li>
+        <li class="active"><strong>Quản lý bình luận</strong></li>
+    </ol>
+    <h3>Quản lý bình luận</h3>
+    <table class="table table-bordered table-striped datatable" id="table-2">
+        <thead>
+        <tr>
